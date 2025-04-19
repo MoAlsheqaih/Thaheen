37758b0bbe2bb22b7a923166159de0692154d66a
AbdulrhmanAmmar20 <s202183170@kfupm.edu.sa>
2025-04-19 16:45:45 +0300

    Implemented dark-mode toggle button.

--- frontend/src/components/Header/DarkModeToggle.jsx
+++ frontend/src/components/Header/DarkModeToggle.jsx
@@ -1,7 +1,54 @@
+import { useEffect, useState } from "react";
+
+import fox_on_moon from "../../assets/fox_on_moon.png";
+import fox_on_sun from "../../assets/fox_on_sun.png";
+
 function DarkModeToggle() {
+  const [theme, setTheme] = useState(() => {
+    if (typeof window !== "undefined") {
+      return localStorage.theme === "dark" ? "dark" : "light";
+    }
+
+    return "light";
+  });
+
+  useEffect(() => {
+    const root = document.documentElement;
+    if (theme === "dark") {
+      root.classList.add("dark");
+      localStorage.theme = "dark";
+    } else {
+      root.classList.remove("dark");
+      localStorage.theme = "light";
+    }
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme(prevTheme => prevTheme === "dark" ? "light" : "dark");
+  };
+
   return (
-    <button className="bg-[#006F6A] hover:bg-[#006f6a]/80 transition-all text-white p-2 rounded-2xl font-semibold drop-shadow-[0_4px_0_#00A59D] text-sm sm:text-base">Theme</button>
-  )
+    <button
+      onClick={toggleTheme}
+      className="bg-[#006F6A] hover:bg-[#006f6a]/80 transition-all text-white p-2 rounded-2xl font-semibold drop-shadow-[0_4px_0_#00A59D] text-sm sm:text-base relative"
+    >
+      <p className="invisible">OO</p>
+
+      {theme === "dark" ? (
+        <img
+          src={fox_on_sun}
+          alt="Light"
+          className="absolute -top-5 left-1/2 -translate-x-1/2 w-12"
+        />
+      ) : (
+        <img
+          src={fox_on_moon}
+          alt="Dark"
+          className="absolute -bottom-8 left-1/2 -translate-x-1/2 w-12"
+        />
+      )}
+    </button>
+  );
 }
 
 export default DarkModeToggle;
